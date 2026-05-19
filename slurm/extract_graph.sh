ec6d81696267d2f77649246a71fe4e7593fe01a6
Toby Penner <tobypenner01@gmail.com>
2026-05-18 21:40:53 -0600

    Add SLURM setup

---
+++ slurm/extract_graph.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=extract_graph
+#SBATCH --output=logs/%j.out
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=192
+#SBATCH --mem=512gb
+
+uv run extract_graph.py
